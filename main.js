fc84776b5ce29d02597748f4d887285113a27107
SoonKhen OwYong <dude@owyong.sk>
2013-06-09 17:13:33 +0800

    initial commit - working version

---
+++ main.js
@@ -0,0 +1,86 @@
+Gmailr.debug = true; // Turn verbose debugging messages on
+
+
+Gmailr.init(function(G) {
+
+	var url = "https://script.google.com/macros/s/AKfycbyMKpGPalHcUAkc29fizWw2VW3Kn_yeMUVdANkIShOb/dev";
+	var newAuth = function() {
+		var height = 200;
+		var width = 300;
+		var top = (screen.height - height)/2;
+		var left = (screen.width - width)/2;
+		var positionString = "height="+height+",width="+width+",top="+top+",left="+left
+		var win = window.open(url,"windowName",positionString);
+	}
+
+	var insertMergeButton = function() {
+		if ($("#btn-merge").length == 0) {
+			var button = '<td class="gU Up"><div class="J-J5-Ji" id=":18m"><div tabindex="1" role="button" id="btn-merge" class="T-I J-J5-Ji aoO T-I-atl L3" style="-moz-user-select: none;">GMerge</div></div></td>';
+			$(".gU.OoRYyc").after(button);
+			insertListener();
+		}
+	}
+
+	var mailSubject = "";
+	var mailTo = "";
+
+	var insertListener = function() {
+		$("#btn-merge").one("click", function(event){
+			event.preventDefault();
+			if ($(".oG.aOy").first().text() == "Saved") {
+				var forgot = "Seems like you forgot your ";
+				if (!mailSubject && !mailTo.length)
+					modalError(forgot + "subject and recipients");
+				else if (!mailSubject)
+					modalError(forgot + "subject");
+				else if (!mailTo.length)
+					modalError(forgot + "recipients");
+				else {
+					$("#btn-merge").text("GMerging");
+					$.ajax({
+						url: url,
+						crossDomain: true,
+						data: {subject:mailSubject},
+						dataType: "jsonp",
+						success: function(data){
+							if (data.status == "failed"){
+								newAuth();
+								$("#btn-merge").text("GMerge");
+								modalError(data.error_message);
+							} else if (data.status == "success") {
+								$('[role="button"][aria-label="Discard draft"]').click()
+								$(".vh").first().text("You have Gmerged like a boss! You have "+data.quota_left+" Gmerge emails left today!");
+							}
+						},
+						error: function(obj, msg, error){
+							// TODO Check the error (possibly using return type) for authenticating or for retrying
+							newAuth();
+							$("#btn-merge").text("GMerge");
+							insertListener();
+						}
+					});
+				}
+			}
+			else
+				modalError("Seems like you clicked too fast, wait for the draft to save first");
+		});
+	}
+
+	var modalError = function(message) {
+		insertListener();
+    dialog = new GMailUI.ModalDialog("Houston, we have a problem!");
+    container = dialog.append(new GMailUI.ModalDialog.Container);
+    footer = dialog.append(new GMailUI.ModalDialog.Footer);
+    okButton = footer.append(new GMailUI.ModalDialog.Button("Aww, my mistake!"));
+    okButton.on('click', dialog.close);
+    container.append(message);
+    dialog.open();
+  }
+
+	G.observe(Gmailr.EVENT_DRAFT_SAVE, function(details) {
+		mailSubject = details.subject;
+		mailTo = details.to;
+		insertMergeButton();
+	});
+
+});
