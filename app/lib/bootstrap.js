d02303b60ddbb2eea6b4a22df21a88249be30b1a
SoonKhen OwYong <dude@owyong.sk>
2013-06-24 23:49:03 +0800

    ah, moved from the cloud back to chrome extension

--- app/lib/bootstrap.js
+++ app/lib/bootstrap.js
@@ -3,5 +3,5 @@ if (top.document === document) {
     test: !(typeof jQuery !== "undefined" && jQuery !== null),
     yep: 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'
   });
-	yepnope([chrome.extension.getURL("lib/underscore-min.js"), chrome.extension.getURL("lib/gmailui.js"), "https://s3.amazonaws.com/GMerge/main-min.js"]);
+	yepnope([chrome.extension.getURL("lib/underscore-min.js"), chrome.extension.getURL("lib/gmailui.js"), chrome.extension.getURL("main.js")]);
 }
